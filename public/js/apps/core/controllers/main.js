16ea4a0b80c40aabfb7c0bfd0b1d19378703fd44
Martin Rabaglia <tinchoforever@gmail.com>
2013-10-12 16:51:45 -0300

    first goal

--- public/js/apps/core/controllers/main.js
+++ public/js/apps/core/controllers/main.js
@@ -3,5 +3,33 @@ var missingio = angular.module('missing-io.controllers', []);
 
 missingio.controller('mainController', function ($scope,$location,profileService) {
 
+  $scope.onSearch =false;
+  $scope.onDetail = false;
+
+  $scope.hidemain = function(){
+    return $scope.onSearch || $scope.onDetail;
+  };
+
+
+
+
+  $scope.backToMain = function(){
+    $scope.onSearch =false;
+    $scope.onDetail = false;
+
+  }
+  $scope.showList = function(){
+    $scope.onSearch = true;
+    $scope.onDetail = false;
+    profileService.getRandom(10, function(data){
+        $scope.list = data;
+    });
+  }
+
+  $scope.showProfile = function(profile){
+    $scope.activeProfile = profile;
+    $scope.onSearch = false;
+    $scope.onDetail = true;
+  }
 
 });
--- public/js/apps/core/services/profiles.js
+++ public/js/apps/core/services/profiles.js
@@ -9,6 +9,20 @@ missingio.service('profileService', function($http) {
             $http.get(url).success(function(data) {
                  callback(data);
             });
+        },
+        getRandom: function(count, callback){
+            var profiles = [];
+            for (var i = 0; i < count; i++) {
+               var p = {
+                 photo: "http://www.missingchildren.org.ar/imagench/leivafedericocarlos2.jpg",
+                 name: 'Federico Carlos Leiva ' + i  + i,
+                 since: '23 de Octubre de 2006',
+                 born: '21 de Junio de 2000',
+                 where: 'Gregorio de Laferrere, Buenos Aires',
+               }
+               profiles.push(p);
+            }
+            callback(profiles)
         }
     };
 });
