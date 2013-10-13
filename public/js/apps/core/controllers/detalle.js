6a1d1da1f12fd6f5cc295b01d7d258dcb03590db
Martin Rabaglia <tinchoforever@gmail.com>
2013-10-13 16:40:50 -0300

    aaa

--- public/js/apps/core/controllers/detalle.js
+++ public/js/apps/core/controllers/detalle.js
@@ -15,6 +15,22 @@ missingio.controller('caseController', function ($scope,$routeParams,$location,p
     $scope.activeProfile =  data;
 
     $scope.onDetail = true;
- })
+ });
+   $scope.tweet = function(){
+
+      var first = $scope.activeProfile.name.split(" ")[0];
+      var link = " http://missing-io.herokuapp.com/encontremos/" + $scope.activeProfile.shortName;
+
+      var text = first + " falta de su casa  " + $scope.activeProfile.formatedSince + ". RT para ayudar a encontrarla "   + "#missingio " +link;
+
+      window.open("https://twitter.com/intent/tweet?original_referer=" + encodeURIComponent(link)+"&source=tweetbutton&text=" + encodeURIComponent(text),
+                  "tweet","location=0,status=0,scrollbars=0, width=780,height=560");
+  };
+  $scope.facebook = function(){
+    var link = " http://missing-io.herokuapp.com/encontremos/" + $scope.activeProfile.shortName;
+    var href="https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(link);
+    window.open(href, "facebook","location=0,status=0,scrollbars=0, width=780,height=560");
+
+  }
 
 });
