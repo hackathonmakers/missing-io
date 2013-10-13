af6badfbb592d93841e90f142b26f47e10078990
Martin Rabaglia <tinchoforever@gmail.com>
2013-10-13 14:50:01 -0300

    golazlo

--- public/js/apps/detalle/controllers/detalle.js
+++ public/js/apps/detalle/controllers/detalle.js
@@ -3,6 +3,22 @@ var missingio = angular.module('missing-io.controllers',[]);
 
 missingio.controller('caseController', function ($scope,$routeParams,$location,profileService) {
 
-  console.log($scope.profile);
+var profile = $scope.profile;
+  if (profile.since || profile.since !== ""){
+        profile.formatedSince = moment(profile.since,"DD/MM/YYYY").fromNow();
+      }
+      if (profile.born || profile.born !== ""){
+        profile.formatedBorn = moment(profile.born,"DD/MM/YYYY").fromNow().replace("hace","");
+      }
+  $scope.tweet = function(){
+
+  }
+  $scope.share = function(){
+
+  }
+  $scope.banners = function(){
+
+  }
+
 
 });
